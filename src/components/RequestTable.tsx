7e202941890aab6b9860fd1237e74d523e1547ba
Radenz <13520166@std.stei.itb.ac.id>
2022-11-26 22:30:55 +0700

    feat: implement table display

---
+++ src/components/RequestTable.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import Table, { Displayable } from "./Table";
+
+interface RequestTableProps {
+  params?: {
+    [key: string]: string;
+  };
+}
+
+export default function RequestTable({ params }: RequestTableProps) {
+  const [data, setData] = useState<Displayable[][] | null>(null);
+  const [error, setError] = useState(null);
+
+  const headers = ["Creator ID", "Subscriber ID", ""];
+
+  useEffect(() => {
+    const API_URL = import.meta.env.VITE_API_URL;
+    fetch(`${API_URL}/subscriptions`)
+      .then((res) => res.json())
+      .then(
+        (res: any[]) => {
+          const data = res.map<Displayable[]>((row) => [
+            row.creatorId,
+            row.subscriberId,
+            // TODO: add accept/reject buttons
+          ]);
+          setData(data);
+        },
+        (err) => {
+          setError(err);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error</div>;
+  } else if (!data) {
+    return <div>Loading...</div>;
+  } else {
+    return <Table headers={headers} data={data} />;
+  }
+}
