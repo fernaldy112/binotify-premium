1c9fbaa7ccd56ccda63326b1ff476a42cc420dee
louisyeee <13520063@std.stei.itb.ac.id>
2022-11-26 20:16:03 +0700

    feat: CRUD functionalities

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,12 +1,106 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 
-// Example of a data array that
-// you might receive from an API
-const data = [
-    { Title: "Siapa aja", Singer_id: 19, audio_path: "entahlah" },
-]
 
-const ManageSong = () => {
+export default function ManageSong() {
+    const [data, setSong] = useState([
+        // TODO: READ DATA DARI DB
+        {
+            id: 1,
+            Title: "Anjay",
+            Singer_id: 10,
+            audio_path: "hehe",
+        },
+        {
+            id: 2,
+            Title: "Turu",
+            Singer_id: 1,
+            audio_path: "hehe",
+        },
+    ])
+    const [file, setFile] = useState<File>();
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            setFile(e.target.files[0]);
+        }
+    };
+    const handleUploadClick = () => {
+
+        if (!file) {
+            return;
+        }
+
+        // 👇 Uploading the file using the fetch API to the server
+        fetch('https://httpbin.org/post', {
+            method: 'POST',
+            body: file,
+            // 👇 Set headers manually for single file upload
+            headers: {
+                'content-type': file.type,
+                'content-length': `${file.size}`, // 👈 Headers need to be a string
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+            .catch((err) => console.error(err));
+    };
+    const [formData, setFormData] = useState({
+        Title: "",
+        audio_path: "",
+    })
+
+
+    function handleSave(e) {
+        let tes = [...data];
+        tes.push({ id: 10, Title: formData.Title, Singer_id: 11, audio_path: file.name });
+        setSong(tes);
+        setEditingId(-1)
+        // TODO: SEND DATA KE DB
+    }
+
+    const [editingId, setEditingId] = useState(-1)
+
+    function handleEdit(value) {
+        setEditingId(value)
+
+    }
+
+    function handleDelete(value) {
+        // TODO: DELETE DARI DB
+    }
+
+    function handleChange(e) {
+        let data = { ...formData };
+        data[e.target.name] = e.target.value;
+        setFormData(data);
+    }
+
+    const listItems = data.map((song) => {
+        if (editingId === song.id) {
+            return (
+                <tr>
+                    <td><input id="songTitle" name="Title" className='border-2 rounded-lg border-black p-1 mb-8' value={formData.Title} onChange={handleChange} type=" text" placeholder='Enter song Title' /></td>
+                    <td>{song.Singer_id}</td>
+                    <td><input id="songAudio" name='songAudio' className='border-2 rounded-lg border-black p-1 mb-8' type="file" onChange={handleFileChange} placeholder='Enter song audio path' /></td>
+                    <td>
+                        <button className='editButton' type='submit' onClick={(e) => { handleSave(song.id) }}>Save</button>
+                        <button className='deleteButton' type='submit' onClick={(e) => { handleDelete(song.id) }}>Delete</button>
+                    </td>
+                </tr>)
+        }
+        else {
+            return (
+                <tr>
+                    <td>{song.Title}</td>
+                    <td>{song.Singer_id}</td>
+                    <td>{song.audio_path}</td>
+                    <td>
+                        <button className='editButton' type='submit' onClick={(e) => { handleEdit(song.id) }}>Edit</button>
+                        <button className='deleteButton' type='submit' onClick={(e) => { handleDelete(song.id) }}>Delete</button>
+                    </td>
+                </tr>)
+        }
+    })
+
     return (
         <div>
             <table>
@@ -16,22 +110,9 @@ const ManageSong = () => {
                     <th>Audio Path</th>
                     <th>Action</th>
                 </tr>
-                {data.map((val, key) => {
-                    return (
-                        <tr key={key}>
-                            <td>{val.Title}</td>
-                            <td>{val.Singer_id}</td>
-                            <td>{val.audio_path}</td>
-                            <td>
-                                <button className='editButton' type='submit'>Edit</button>
-                                <button className='deleteButton' type='submit'>Delete</button>
-                            </td>
-                        </tr>
-                    )
-                })}
+                {listItems}
             </table>
         </div>
     );
 }
 
-export default ManageSong
--- src/components/Song.tsx
+++ src/components/Song.tsx
@@ -1,10 +1,36 @@
 import React, { ChangeEvent, useState } from 'react'
-import List from "./List";
+import ManageSong from "./List";
 
 
 const SongPage = () => {
     const [file, setFile] = useState<File>();
+    const [songData, setSong] = useState([
+        {
+            id: 1,
+            Title: "Anjay",
+            Singer_id: 10,
+            audio_path: "hehe",
+        },
+        {
+            id: 2,
+            Title: "Turu",
+            Singer_id: 1,
+            audio_path: "hehe",
+        },
+    ])
 
+    const [formData, setFormData] = useState({
+        Title: "",
+        audio_path: "",
+    })
+
+    function handleChange(e) {
+        let data = { ...formData };
+        data[e.target.name] = e.target.value;
+        setFormData(data);
+    }
+
+    const [isUpdate, setIsUpdate] = useState({ id: null, status: false });
     const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
         if (e.target.files) {
             setFile(e.target.files[0]);
@@ -12,6 +38,7 @@ const SongPage = () => {
     };
 
     const handleUploadClick = () => {
+
         if (!file) {
             return;
         }
@@ -30,14 +57,27 @@ const SongPage = () => {
             .then((data) => console.log(data))
             .catch((err) => console.error(err));
     };
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        alert("Add new song?");
+
+        let data = [...songData];
+        data.push({ id: 10, Title: formData.Title, Singer_id: 11, audio_path: file.name });
+        setSong(data);
+        console.log(data);
+        // TODO: Masukkan ke DB
+    }
+
+
     return (
         <div className='w-full max-w-xl flex flex-col justify-center mx-auto my-10'>
             <div className='border-2 border-black rounded-lg'>
-                <form id="insertSongForm" className='p-10 flex flex-col items-center justify-items-stretch pb-6' action="">
+                <form id="insertSongForm" onSubmit={handleSubmit} className='p-10 flex flex-col items-center justify-items-stretch pb-6' action="" onSubmit={handleSubmit}>
                     <h2 className='w-full mx-auto text-center text-5xl font-bold mb-10'>Song</h2>
                     <div className='w-full'>
                         <label htmlFor="songTitle" className='w-full text-left block'>Title</label>
-                        <input id="songTitle" name='songTitle' className='border-2 rounded-lg border-black p-1 w-full mb-8' type="text" placeholder='Enter song Title' />
+                        <input id="songTitle" name="Title" className='border-2 rounded-lg border-black p-1 w-full mb-8' value={formData.Title} onChange={handleChange} type=" text" placeholder='Enter song Title' />
                     </div>
                     <div className='w-full'>
                         <label htmlFor="songAudio" className='w-full text-left block'>Audio Path</label>
@@ -48,10 +88,9 @@ const SongPage = () => {
                     <button className='border-2 rounded-lg border-black p-1 w-full max-w-[100px]' type='submit' onClick={handleUploadClick}>Insert Song</button>
                 </form>
                 <hr />
-            </div>
+            </div >
             <br></br>
-
-        </div>
+        </div >
     )
 }
 
