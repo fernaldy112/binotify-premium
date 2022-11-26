475a5ec0b8fc70d9ed0808b15c8025a83bc25fb9
Radenz <13520166@std.stei.itb.ac.id>
2022-11-26 22:30:55 +0700

    feat: implement table display

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,35 +1,42 @@
-type Displayable = string | number;
+import { ReactNode } from "react";
+
+export type Displayable = string | number | ReactNode;
 
 interface TableProps {
-  headers: string[],
-  data: Displayable[][],
+  headers: string[];
+  data: Displayable[][];
 }
 
 interface TableRowProps {
-  data: Displayable[]
+  data: Displayable[];
 }
 
-function TableRow({data}: TableRowProps) {
+function TableRow({ data }: TableRowProps) {
   return (
     <tr>
-      {data.map(cell => <td>{cell}</td>)}
+      {data.map((cell) => (
+        <td>{cell}</td>
+      ))}
     </tr>
   );
 }
 
 export default function Table(props: TableProps) {
-
-  const {headers, data} = props;
+  const { headers, data } = props;
 
   return (
     <table>
       <thead>
         <tr>
-          {headers.map(header => <th>{header}</th>)}
+          {headers.map((header) => (
+            <th>{header}</th>
+          ))}
         </tr>
       </thead>
       <tbody>
-        {data.map(row => <TableRow data={row} />)}
+        {data.map((row) => (
+          <TableRow data={row} />
+        ))}
       </tbody>
     </table>
   );
