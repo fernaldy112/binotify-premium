d6297bc8af7a166d655b40e8db2a68aa314c4b34
Radenz <13520166@std.stei.itb.ac.id>
2022-11-26 22:32:49 +0700

    refactor: remove unnecessary type

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,14 +1,12 @@
 import { ReactNode } from "react";
 
-export type Displayable = string | number | ReactNode;
-
 interface TableProps {
   headers: string[];
-  data: Displayable[][];
+  data: ReactNode[][];
 }
 
 interface TableRowProps {
-  data: Displayable[];
+  data: ReactNode[];
 }
 
 function TableRow({ data }: TableRowProps) {
