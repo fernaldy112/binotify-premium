503f7be09a09c2821e329dbb832fffa84776f399
Radenz <13520166@std.stei.itb.ac.id>
2022-11-27 02:33:49 +0700

    feat: add header component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1DB954",
+        graybg: "#171717",
+      },
+      borderWidth: {
+        1: "1px",
+      },
+    },
   },
   plugins: [],
 };
