896a31159d26700ee906ecb1bd420d98bba216cf
Radenz <13520166@std.stei.itb.ac.id>
2022-11-26 10:08:33 +0700

    feat: add table components

---
+++ src/components/Table.tsx
@@ -0,0 +1,36 @@
+type Displayable = string | number;
+
+interface TableProps {
+  headers: string[],
+  data: Displayable[][],
+}
+
+interface TableRowProps {
+  data: Displayable[]
+}
+
+function TableRow({data}: TableRowProps) {
+  return (
+    <tr>
+      {data.map(cell => <td>{cell}</td>)}
+    </tr>
+  );
+}
+
+export default function Table(props: TableProps) {
+
+  const {headers, data} = props;
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {headers.map(header => <th>{header}</th>)}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(row => <TableRow data={row} />)}
+      </tbody>
+    </table>
+  );
+}
